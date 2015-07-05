71d3d0eadc000a5312c324fff5990a5f148149b5
juco <julian@juco.co.uk>
2015-07-05 18:10:38 +0100

    ng-html2js

--- test/unit/karma-options.js
+++ test/unit/karma-options.js
@@ -1,28 +1,37 @@
-module.exports = function(config){
+module.exports = function(config) {
   config.set({
+    basePath: '../../',
 
-    basePath : '../../',
+    preprocessors: {
+      'app/views/partial/**/*.html': 'ng-html2js'
+   },
 
-    files : [
-      'vendor/angular/angular.js',
-      'vendor/angular-route/angular-route.js',
-      'vendor/angular-mocks/angular-mocks.js',
-      'app/js/**/*.js',
-      'test/unit/**/*-test.js'
-    ],
+   files: [
+     'vendor/angular/angular.js',
+     'vendor/angular-route/angular-route.js',
+     'vendor/angular-mocks/angular-mocks.js',
+     'app/js/**/*.js',
+     'test/unit/**/*-test.js',
+     'app/**/*.html'
+   ],
 
-    autoWatch : true,
+    ngHtml2JsPreprocessor: {
+      moduleName: 'juco.movies.partials',
+      stripPrefix: 'app'
+    },
 
-    frameworks: ['jasmine'],
+   autoWatch: true,
 
-    browsers : ['PhantomJS'],
+   frameworks: ['jasmine'],
 
-    plugins : [
-            'karma-phantomjs-launcher',
-            'karma-jasmine'
-            ],
+   browsers: ['PhantomJS'],
 
-    reporters: ['progress']
+   plugins: [
+     'karma-phantomjs-launcher',
+     'karma-jasmine',
+     'karma-ng-html2js-preprocessor'
+   ],
 
+   reporters: ['progress']
   });
 };
